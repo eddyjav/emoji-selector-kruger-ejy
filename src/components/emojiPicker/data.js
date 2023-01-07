db8eca69556a8e5b9ad48d27c36a768059932a1c
eddyjav <eddy.j.av@gmail.com>
2023-01-07 16:17:30 -0500

    :smiley: add data

---
+++ src/components/emojiPicker/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    symbol: "🙂",
+    name: "cara alegre",
+    keywords: "smiling face happpy",
+  },
+  {
+    symbol: "💞",
+    name: "corazon",
+    keywords: "heart love like",
+  },
+  {
+    symbol: "🔥",
+    name: "fuego",
+    keywords: "fire light hot burn",
+  },
+  {
+    symbol: "💎",
+    name: "diamante",
+    keywords: "diamond gem stone",
+  },
+  {
+    symbol: "🤯",
+    name: "cabeza explotada",
+    keywords: "head exploding mind blowing",
+  },
+];
